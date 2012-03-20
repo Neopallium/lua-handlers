c42cce7c28229144b633937f4bda0b339a24fc2c
Robert G. Jakabosky <bobby@sharedrealm.com>
2012-03-20 00:49:41 -0700

    Fix bug from "multiple poller support commit".

--- handler/http/client/hosts.lua
+++ handler/http/client/hosts.lua
@@ -105,7 +105,7 @@ function host_mt:queue_request(req)
 		end
 		-- no pooled connection, create a new connection.
 		local err
-		conn, err = httpconnection(self.client, self)
+		conn, err = httpconnection(self)
 		if conn == nil then return false, err end
 		tinsert(self.connections, conn)
 	end
