39fab0b357871d072a01feb8625199323b770646
Bjoern Kalkbrenner <bjoern.kalkbrenner@belnet.de>
2014-11-06 13:22:29 +0100

    fixes problem when tls write backend blocks

--- handler/connection/tls_backend.lua
+++ handler/connection/tls_backend.lua
@@ -150,7 +150,7 @@ local function sock_send_data(self, buf)
 	local num, errno, err = sock:send(buf)
 	if not num then
 		-- got timeout error block writes.
-		if num == false then
+		if num == false or SSL_ERROR_WANT_WRITE then
 			-- got EAGAIN
 			is_blocked = true
 		else -- data == nil
